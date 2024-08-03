8f3cc83a42265995777adb81e6c573377acc92e4
Samoy <samoy_young@163.com>
2024-08-03 17:06:01 +0800

    chore: 环境搭建

---
+++ README.md
@@ -0,0 +1,7 @@
+# 乐尚代驾学习项目
+
+根据B站[尚硅谷课程](https://www.bilibili.com/video/BV1nW421R7qJ)所讲述的内容，自己实现的代码。
+
+## 环境
+
+使用`docker compose`进行环境安装。
---
+++ service/service-coupon/src/main/java/com/atguigu/daijia/coupon/service/impl/CouponInfoServiceImpl.java
@@ -0,0 +1,13 @@
+package com.atguigu.daijia.coupon.service.impl;
+
+import com.atguigu.daijia.coupon.mapper.CouponInfoMapper;
+import com.atguigu.daijia.coupon.service.CouponInfoService;
+import com.atguigu.daijia.model.entity.coupon.CouponInfo;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CouponInfoServiceImpl extends ServiceImpl<CouponInfoMapper, CouponInfo> implements CouponInfoService {
+
+
+}
