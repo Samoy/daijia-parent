d9cd8845a216e51a99cf142a2f2c2dcdcd00d115
Samoy <samoy_young@163.com>
2024-08-29 18:05:57 +0800

    feat: 录音上传

--- service-client/service-order-client/src/main/java/com/atguigu/daijia/order/client/OrderMonitorFeignClient.java
+++ service-client/service-order-client/src/main/java/com/atguigu/daijia/order/client/OrderMonitorFeignClient.java
@@ -1,10 +1,21 @@
 package com.atguigu.daijia.order.client;
 
+import com.atguigu.daijia.common.result.Result;
+import com.atguigu.daijia.model.entity.order.OrderMonitorRecord;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @FeignClient(value = "service-order")
 public interface OrderMonitorFeignClient {
 
-
+    /**
+     * 保存订单监控记录数据
+     *
+     * @param orderMonitorRecord 监控数据
+     * @return 保存结果
+     */
+    @PostMapping("/order/monitor/saveOrderMonitorRecord")
+    Result<Boolean> saveMonitorRecord(@RequestBody OrderMonitorRecord orderMonitorRecord);
 }
--- web/web-driver/src/main/java/com/atguigu/daijia/driver/service/impl/MonitorServiceImpl.java
+++ web/web-driver/src/main/java/com/atguigu/daijia/driver/service/impl/MonitorServiceImpl.java
@@ -1,13 +1,39 @@
 package com.atguigu.daijia.driver.service.impl;
 
+import com.atguigu.daijia.common.execption.GuiguException;
+import com.atguigu.daijia.common.result.Result;
+import com.atguigu.daijia.common.result.ResultCodeEnum;
+import com.atguigu.daijia.driver.service.FileService;
 import com.atguigu.daijia.driver.service.MonitorService;
+import com.atguigu.daijia.model.entity.order.OrderMonitorRecord;
+import com.atguigu.daijia.model.form.order.OrderMonitorForm;
+import com.atguigu.daijia.order.client.OrderMonitorFeignClient;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @Service
-@SuppressWarnings({"unchecked", "rawtypes"})
 public class MonitorServiceImpl implements MonitorService {
 
+    @Resource
+    private FileService fileService;
 
+    @Resource
+    private OrderMonitorFeignClient orderMonitorFeignClient;
+
+    @Override
+    public Boolean upload(MultipartFile file, OrderMonitorForm orderMonitorForm) {
+        String url = fileService.upload(file);
+        OrderMonitorRecord orderMonitorRecord = new OrderMonitorRecord();
+        orderMonitorRecord.setOrderId(orderMonitorForm.getOrderId());
+        orderMonitorRecord.setFileUrl(url);
+        orderMonitorRecord.setContent(orderMonitorForm.getContent());
+        Result<Boolean> result = orderMonitorFeignClient.saveMonitorRecord(orderMonitorRecord);
+        if (!ResultCodeEnum.SUCCESS.getCode().equals(result.getCode())) {
+            throw new GuiguException(ResultCodeEnum.FEIGN_FAIL);
+        }
+        return result.getData();
+    }
 }
